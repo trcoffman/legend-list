9c513b22c35b185d3c44f267495ae6f178b6300b
Tyler Coffman <tyler.coffman@appfolio.com>
2026-02-18 14:27:54 -0800

    +html.tsx

---
+++ example/app/+html.tsx
@@ -0,0 +1,19 @@
+import { ScrollViewStyleReset } from "expo-router/html";
+import type { PropsWithChildren } from "react";
+
+export default function Root({ children }: PropsWithChildren) {
+    return (
+        <html lang="en">
+            <head>
+                <meta charSet="utf-8" />
+                <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, shrink-to-fit=no"
+                />
+                <ScrollViewStyleReset />
+            </head>
+            <body>{children}</body>
+        </html>
+    );
+}
